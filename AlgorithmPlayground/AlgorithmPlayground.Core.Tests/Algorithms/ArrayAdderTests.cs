27b5fef8278fd26648f78ed3cb43c996732fa18a
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-01 20:44:19 +0200

    Add array sum algorithms in O(n) and O(1) versions

---
+++ AlgorithmPlayground/AlgorithmPlayground.Core.Tests/Algorithms/ArrayAdderTests.cs
@@ -0,0 +1,33 @@
+﻿using AlgorithmPlayground.Core.Algorithms;
+using Xunit;
+
+namespace AlgorithmPlayground.Core.Tests.Algorithms;
+
+public class ArrayAdderTests
+{
+    [Fact]
+    public void NaiveArraySum_ShouldReturnCorrectResult_WhenCalled()
+    {
+        // Arrange
+        var array = new[] { 8, 23, -12, 0, 14, 3, 21};
+
+        // Act
+        var sum = ArrayAdder.NaiveArraySum(array);
+
+        // Assert
+        Assert.Equal(57, sum);
+    }
+
+    [Theory]
+    [InlineData(55, new [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})]
+    [InlineData(28, new [] {1, 5, 9, 13})]
+    public void ArithmeticSequenceSum_ShouldReturnCorrectResult_WhenCalled(int expectedSum, int[] sequence)
+    {
+        // Arrange
+        // Act
+        var sum = ArrayAdder.ArithmeticSequenceSum(sequence);
+
+        // Assert
+        Assert.Equal(expectedSum, sum);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.Core/Algorithms/ArrayAdder.cs
@@ -0,0 +1,34 @@
+﻿namespace AlgorithmPlayground.Core.Algorithms;
+
+public static class ArrayAdder
+{
+    public static int NaiveArraySum(IEnumerable<int> array)
+    {
+        // Time complexity: O(n)
+        var sum = 0;
+        foreach (var number in array)
+        {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    public static int ArithmeticSequenceSum(int[] array)
+    {
+        // Time complexity: O(1)
+        if (array.Length == 0)
+        {
+            return 0;
+        }
+
+        if (array.Length == 1)
+        {
+            return array[0];
+        }
+
+        var difference = array[1] - array[0];
+        var count = array.Length;
+        return count / 2 * (2 * array[0] + (count - 1) * difference);
+    }
+}
