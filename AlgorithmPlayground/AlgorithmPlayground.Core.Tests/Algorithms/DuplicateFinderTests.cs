3da36b0931844ed4cf41eb593a3f68b823c41410
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-02 20:28:19 +0200

    Add straightforward implementation of checking for duplicates in array

---
+++ AlgorithmPlayground/AlgorithmPlayground.Core.Tests/Algorithms/DuplicateFinderTests.cs
@@ -0,0 +1,20 @@
+﻿using AlgorithmPlayground.Core.Algorithms;
+using Xunit;
+
+namespace AlgorithmPlayground.Core.Tests.Algorithms;
+
+public class DuplicateFinderTests
+{
+    [Theory]
+    [InlineData(false, new[] {1, 2, 3, 4, 5, 6})]
+    [InlineData(true, new[] {1, 10, 11, 12, 13, 1})]
+    [InlineData(true, new[] {0, 0})]
+    public void NaiveCheckForDuplicates_ShouldReturnExpectedResult_WhenCalled(bool expectedResult, int[] array)
+    {
+        // Arrange
+        // Act
+        var result = DuplicateFinder.NaiveCheckForDuplicates(array);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.Core/Algorithms/DuplicateFinder.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmPlayground.Core.Algorithms;
+
+public static class DuplicateFinder
+{
+    public static bool NaiveCheckForDuplicates(int[] array)
+    {
+        // O(n^2)...
+        for (var i = 0; i < array.Length; i++)
+        {
+            var currentNumber = array[i];
+            for (var j = 0; j < array.Length; j++)
+            {
+                if (i != j)
+                {
+                    var otherNumber = array[j];
+                    if (otherNumber == currentNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
