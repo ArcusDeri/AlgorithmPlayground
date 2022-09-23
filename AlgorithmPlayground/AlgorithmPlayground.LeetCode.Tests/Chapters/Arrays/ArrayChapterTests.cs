e60c572bff44f4322fe2b9b334dd0dd9cd473af6
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-23 11:20:06 +0200

    Implement solution for Third Maximum Number problem

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Arrays/ArrayChapterTests.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Arrays/ArrayChapterTests.cs
@@ -207,4 +207,20 @@ public class ArrayChapterTests
         // Assert
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [InlineData(1, new[] {3, 2, 1})]
+    [InlineData(2, new[] {1, 2})]
+    [InlineData(1, new[] {2, 2, 3, 1})]
+    [InlineData(2, new[] {1, 2, 2, 5, 3, 5})]
+    public void _16_ThirdMaximumNumber_ShouldReturnExpectedResult(int expected, int[] nums)
+    {
+        // Arrange
+        // Act
+        var result = _16_ThirdMaximumNumber.ThirdMax(nums);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
 }
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Arrays/_16_ThirdMaximumNumber.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Arrays;
+
+public static class _16_ThirdMaximumNumber
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3231/
+    /// </summary>
+    public static int ThirdMax(int[] nums)
+    {
+        int? max1 = null, max2 = null, max3 = null;
+        foreach (var num in nums)
+        {
+            if (num == max1 || num == max2 || num == max3)
+            {
+                continue;
+            }
+
+            if (max1 == null || num > max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+            else if (max2 == null || num > max2)
+            {
+                max3 = max2;
+                max2 = num;
+            }
+            else if (max3 == null || num > max3)
+            {
+                max3 = num;
+            }
+        }
+
+        return max3 ?? (max1 ?? 0);
+    }
+}
