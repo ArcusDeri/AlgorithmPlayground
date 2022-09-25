e7f2ecd76ec00a9a9f6d46fcfa5d079538f98eb9
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-25 22:01:03 +0200

    Add implementation for Find All Numbers Disappeared in an Array problem

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Arrays/ArrayChapterTests.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Arrays/ArrayChapterTests.cs
@@ -223,4 +223,16 @@ public class ArrayChapterTests
         Assert.Equal(expected, result);
     }
 
+    [Theory]
+    [InlineData(new[] {5, 6}, new[] {4, 3, 2, 7, 8, 2, 3, 1})]
+    [InlineData(new[] {2}, new[] {1, 1})]
+    public void _17_FindAllNumbersDisappearedInAnArray_ShouldReturnExpectedResult(int[] expected, int[] nums)
+    {
+        // Arrange
+        // Act
+        var result = _17_FindAllNumbersDisappearedInAnArray.FindDisappearedNumbers(nums);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
 }
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Arrays/_17_FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Arrays;
+
+public static class _17_FindAllNumbersDisappearedInAnArray
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/
+    /// Solution with O(n) time complexity and O(1) space complexity.
+    /// </summary>
+    public static IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        var result = new List<int>(nums.Length);
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var indexOfFoundElement = Math.Abs(nums[i]) - 1;
+            if (nums[indexOfFoundElement] < 0)
+            {
+                continue;
+            }
+            nums[indexOfFoundElement] *= -1;
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                result.Add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
