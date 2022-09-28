dcb71b391f1a6d1d3a423bbcfffa93cbf55657d3
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-28 20:33:33 +0200

    Solve Running Sum of 1D array problem

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/BeginnersGuideTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.BeginnersGuide;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters;
+
+public class BeginnersGuideTests
+{
+    [Theory]
+    [InlineData(new[] {1, 3, 6, 10}, new[] {1, 2, 3, 4})]
+    [InlineData(new[] {1, 2, 3, 4, 5}, new[] {1, 1, 1, 1, 1})]
+    [InlineData(new[] {3, 4, 6, 16, 17}, new[] {3, 1, 2, 10, 1})]
+    public void _1_1480_RunningSum_ShouldReturnExpectedResult(int[] expected, int[] nums)
+    {
+        // Arrange
+        // Act
+        var result = _1_1480_RunningSumOf1DArray.RunningSum(nums);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/BeginnersGuide/_1_1480_RunningSumOf1DArray.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.BeginnersGuide;
+
+public static class _1_1480_RunningSumOf1DArray
+{
+    /// <summary>
+    /// https://leetcode.com/problems/running-sum-of-1d-array/
+    /// </summary>
+    public static int[] RunningSum(int[] nums)
+    {
+        var runningSum = new int[nums.Length];
+        runningSum[0] = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            runningSum[i] = runningSum[i - 1] + nums[i];
+        }
+
+        return runningSum;
+    }
+}
