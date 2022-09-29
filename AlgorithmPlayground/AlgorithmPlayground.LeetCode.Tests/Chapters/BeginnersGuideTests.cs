5edea827d302adabf37b5451898414a27d40d866
Marcin Michniak <marcin.michniak@outlook.com>
2022-09-29 18:14:44 +0200

    Solve 1672. Richest Customer Wealth problem

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/BeginnersGuideTests.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/BeginnersGuideTests.cs
@@ -18,4 +18,18 @@ public class BeginnersGuideTests
         // Assert
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [InlineData(6, new[] {1, 2, 3}, new[] {3, 2, 1})]
+    [InlineData(10, new[] {1, 5}, new[] {7, 3}, new[] {3, 5})]
+    [InlineData(17, new[] {2, 8, 7}, new[] {7, 1, 3}, new[] {1, 9, 5})]
+    public void _2_1672_MaximumWealth_ShouldReturnExpectedResult(int expectedResult, params int[][] accounts)
+    {
+        // Arrange
+        // Act
+        var result = _2_1672_RichestCustomerWealth.MaximumWealth(accounts);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
 }
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/BeginnersGuide/_2_1672_RichestCustomerWealth.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.BeginnersGuide;
+
+public static class _2_1672_RichestCustomerWealth
+{
+    public static int MaximumWealth(int[][] accounts)
+    {
+        int max = 0;
+        foreach (var account in accounts)
+        {
+            var currentAccountSum = 0;
+            foreach (var amount in account)
+            {
+                currentAccountSum += amount;
+            }
+
+            max = currentAccountSum > max ? currentAccountSum : max;
+        }
+
+        return max;
+    }
+}
