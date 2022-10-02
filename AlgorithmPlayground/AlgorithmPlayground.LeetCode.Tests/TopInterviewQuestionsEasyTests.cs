b3bc53120f7093e0fbc48bc88d41e8d413fceff6
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-02 09:51:58 +0200

    Solve Best Time to Buy and Sell Stocks II

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/TopInterviewQuestionsEasyTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.TopInterviewQuestionsEasy;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests;
+
+public class TopInterviewQuestionsEasyTests
+{
+    [Theory]
+    [InlineData(7, new[] {7, 1, 5, 3, 6, 4})]
+    [InlineData(4, new[] {1, 2, 3, 4, 5})]
+    [InlineData(0, new[] {7, 6, 4, 3, 1})]
+    public void _2_MaxProfit_ShouldReturnExpectedResult(int expected, int[] prices)
+    {
+        // Arrange
+        // Act
+        var result = _2_BestTimeToBuyAndSellStockII.MaxProfit(prices);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/TopInterviewQuestionsEasy/_2_BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmPlayground.LeetCode.TopInterviewQuestionsEasy;
+
+public static class _2_BestTimeToBuyAndSellStockII
+{
+    /// <summary>
+    /// https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/
+    /// </summary>
+    public static int MaxProfit(int[] prices)
+    {
+        var profit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            var potentialProfit = prices[i] - prices[i - 1];
+            if (potentialProfit > 0)
+            {
+                profit += potentialProfit;
+            }
+        }
+
+        return profit;
+    }
+}
