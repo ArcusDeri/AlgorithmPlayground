efdeb82029f836fab58a7764540ba86ff25d081e
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-05 21:17:24 +0200

    Solve Linked List Cycle II

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_3_LinkedListCycleII.cs
@@ -0,0 +1,32 @@
+﻿using AlgorithmPlayground.LeetCode.Model;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+
+public static class _3_LinkedListCycleII
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+    /// </summary>
+    public static ListNode? DetectCycle(ListNode head)
+    {
+        var walker = head;
+        var runner = head;
+        while (runner?.next is { })
+        {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner)
+            {
+                walker = head;
+                while (walker != runner)
+                {
+                    walker = walker.next;
+                    runner = runner.next;
+                }
+
+                return walker;
+            }
+        }
+        return null;
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_4_IntersectionOfTwoLinkedList.cs
@@ -0,0 +1,14 @@
+﻿using AlgorithmPlayground.LeetCode.Model;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+
+public static class _4_IntersectionOfTwoLinkedList
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
+    /// </summary>
+    public static ListNode GetIntersectionNode(ListNode listA, ListNode listB)
+    {
+        throw new NotImplementedException();
+    }
+}
