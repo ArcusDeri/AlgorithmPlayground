55c565e9dc9f70deeb599ae6567585406fd46c5d
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-07 15:52:21 +0200

    Solve Intersection of Two Linked Lists

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_4_IntersectionOfTwoLinkedList.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_4_IntersectionOfTwoLinkedList.cs
@@ -7,8 +7,52 @@ public static class _4_IntersectionOfTwoLinkedList
     /// <summary>
     /// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
     /// </summary>
-    public static ListNode GetIntersectionNode(ListNode listA, ListNode listB)
+    public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
     {
-        throw new NotImplementedException();
+        var hasInterSection = false;
+        var currentA = headA;
+        var currentB = headB;
+        int aLength = 0, bLength = 0;
+
+        while (currentA is {})
+        {
+            aLength++;
+            currentA = currentA.next;
+        }
+        while (currentB is {})
+        {
+            bLength++;
+            currentB = currentB.next;
+        }
+
+        var lengthDifference = aLength - bLength;
+        var listToShift = headA;
+        var notShiftedList = headB;
+        if (lengthDifference < 0)
+        {
+            listToShift = headB;
+            notShiftedList = headA;
+            lengthDifference *= -1;
+        }
+
+        var position = 0;
+        while (position < lengthDifference)
+        {
+            listToShift = listToShift.next;
+            position++;
+        }
+
+        while (listToShift != notShiftedList)
+        {
+            if (listToShift is null || notShiftedList is null)
+            {
+                return null;
+            }
+
+            listToShift = listToShift.next;
+            notShiftedList = notShiftedList.next;
+        }
+
+        return listToShift;
     }
 }
