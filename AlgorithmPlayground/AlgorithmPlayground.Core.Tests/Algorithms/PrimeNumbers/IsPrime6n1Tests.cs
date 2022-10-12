716842283fd8bd8083787d7158b6763fffdc4035
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-12 17:30:05 +0200

    Implement algorithm for checking primality of a number

---
+++ AlgorithmPlayground/AlgorithmPlayground.Core.Tests/Algorithms/PrimeNumbers/IsPrime6n1Tests.cs
@@ -0,0 +1,31 @@
+﻿using AlgorithmPlayground.Core.Algorithms.PrimeNumbers;
+using Xunit;
+
+namespace AlgorithmPlayground.Core.Tests.Algorithms.PrimeNumbers;
+
+public class IsPrime6N1Tests
+{
+    [Theory]
+    [InlineData(true, 2)]
+    [InlineData(true, 3)]
+    [InlineData(true, 5)]
+    [InlineData(true, 11)]
+    [InlineData(true, 13)]
+    [InlineData(true, 17)]
+    [InlineData(true, 67)]
+    [InlineData(true, 97)]
+    [InlineData(false, 0)]
+    [InlineData(false, 1)]
+    [InlineData(false, 4)]
+    [InlineData(false, 20)]
+    [InlineData(false, 21)]
+    public void IsPrime_ShouldReturnExpectedResult_ForGivenNumber(bool expectedResult, int n)
+    {
+        // Arrange
+        // Act
+        var result = IsPrime6N1.IsPrime(n);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.Core/Algorithms/PrimeNumbers/IsPrime6N1.cs
@@ -0,0 +1,38 @@
+﻿namespace AlgorithmPlayground.Core.Algorithms.PrimeNumbers;
+
+public static class IsPrime6N1
+{
+    /// <summary>
+    /// This algorithm leverages the fact, that any prime number larger than 3 can be
+    /// represented in form 6n+-1 (where n is a natural number). This method is called
+    /// "trial division".
+    /// </summary>
+    public static bool IsPrime(int n)
+    {
+        switch (n)
+        {
+            case <= 1:
+                return false;
+            case <= 3:
+                return true;
+        }
+
+        if (n % 2 == 0 || n % 3 == 0)
+        {
+            return false;
+        }
+
+        for (var i = 5; i * i <= n; i += 6)
+        {
+            // With this condition we omit numbers divisible by 2 or 3,
+            // in other words, we check if n is divisible by other prime number less
+            // less than n.
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
