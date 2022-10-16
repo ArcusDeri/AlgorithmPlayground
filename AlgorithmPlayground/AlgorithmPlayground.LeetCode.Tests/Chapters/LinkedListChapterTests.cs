a95b4fd4a04d6c658e60b2511ede3e90cf92998e
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-16 20:38:39 +0200

    Add tests for Design Doubly Linked List

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/LinkedListChapterTests.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/LinkedListChapterTests.cs
@@ -449,4 +449,65 @@ public class LinkedListChapterTests
         // Assert
         Assert.False(result);
     }
+
+    [Fact]
+    public void _10_DesignDoublyLinkedList_ShouldHaveExpectedShape_ForScenario1()
+    {
+        // Arrange
+        var list = new _10_DesignDoublyLinkedList();
+        const int expectedHeadVal = -1;
+
+        // Act
+        list.AddAtHead(1);
+        list.AddAtTail(3);
+        list.AddAtIndex(1, 2);
+        list.DeleteAtIndex(1);
+        list.DeleteAtIndex(3);
+        list.DeleteAtIndex(0);
+        list.DeleteAtIndex(0);
+
+        // Assert
+        Assert.Equal(expectedHeadVal, list.Get(0));
+    }
+
+    [Fact]
+    public void _10_DesignDoublyLinkedList_ShouldHaveExpectedShape_ForScenario2()
+    {
+        // Arrange
+        var list = new _10_DesignDoublyLinkedList();
+        const int expectedHeadVal = 1;
+        const int expectedTailVal = 3;
+        const int expectedTailIndex = 1;
+
+        // Act
+        list.AddAtHead(1);
+        list.AddAtTail(3);
+        list.AddAtIndex(1, 2);
+        list.DeleteAtIndex(1);
+
+        // Assert
+        Assert.NotNull(list);
+        Assert.Equal(expectedHeadVal, list.Get(0));
+        Assert.Equal(expectedTailVal, list.Get(expectedTailIndex));
+    }
+
+    [Fact]
+    public void _10_DesignDoublyLinkedList_ShouldHaveExpectedShape_ForScenario3()
+    {
+        // Arrange
+        var list = new _10_DesignDoublyLinkedList();
+        const int expectedHeadVal = 1;
+        const int expectedTailVal = 3;
+        const int expectedTailIndex = 1;
+
+        // Act
+        list.AddAtHead(1);
+        list.AddAtTail(3);
+        list.AddAtIndex(3, 2);
+
+        // Assert
+        Assert.NotNull(list);
+        Assert.Equal(expectedHeadVal, list.Get(0));
+        Assert.Equal(expectedTailVal, list.Get(expectedTailIndex));
+    }
 }
