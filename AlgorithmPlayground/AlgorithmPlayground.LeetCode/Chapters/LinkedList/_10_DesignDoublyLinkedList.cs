793b054fef49f67ad582896ec7f79133dc76e280
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-17 17:55:16 +0200

    Fix inaccuracies in doubly linked list implementation

--- AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_10_DesignDoublyLinkedList.cs
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_10_DesignDoublyLinkedList.cs
@@ -17,7 +17,12 @@ public class _10_DesignDoublyLinkedList
         while (i < index)
         {
             i++;
-            current = current.next;
+            current = current?.next;
+        }
+
+        if (current is null)
+        {
+            return -1;
         }
 
         return current.val;
@@ -36,6 +41,11 @@ public class _10_DesignDoublyLinkedList
 
     public void AddAtTail(int val)
     {
+        if (_head is null)
+        {
+            AddAtHead(val);
+            return;
+        }
         var current = _head;
         while (current?.next is {})
         {
@@ -59,12 +69,20 @@ public class _10_DesignDoublyLinkedList
         while (i < index)
         {
             i++;
-            current = current.next;
+            current = current?.next;
         }
 
-        var newNode = new DoubleListNode(val);
-        newNode.prev = current;
-        newNode.next = current.next;
+        if (current is null)
+        {
+            // invalid index, do nothing
+            return;
+        }
+
+        var newNode = new DoubleListNode(val)
+        {
+            prev = current,
+            next = current.next
+        };
         if (newNode.next is { })
         {
             newNode.next.prev = newNode;
@@ -78,16 +96,23 @@ public class _10_DesignDoublyLinkedList
         var current = _head;
         if (index == 0)
         {
-            _head = _head.next;
+            _head = _head?.next;
+            return;
         }
 
-        while (i < index)
+        while (i < index - 1)
         {
             i++;
-            current = current.next;
+            current = current?.next;
         }
 
-        current.next = current.next.next;
+        if (current is null)
+        {
+            // invalid index, do nothing
+            return;
+        }
+
+        current.next = current.next?.next;
         if (current.next is { })
         {
             current.next.prev = current;
