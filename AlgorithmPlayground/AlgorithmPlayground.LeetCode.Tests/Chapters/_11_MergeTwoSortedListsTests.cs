5ab9cd652e39f5fc2554d5122370831200977a91
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-18 17:57:12 +0200

    Solve Merge Two Sorted Lists

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/_11_MergeTwoSortedListsTests.cs
@@ -0,0 +1,60 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+using AlgorithmPlayground.LeetCode.Model;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters;
+
+public class _11_MergeTwoSortedListsTests
+{
+    [Fact]
+    public void MergeTwoLists_ShouldReturnListOfExpectedShape_ForTwoPopulatedLists()
+    {
+        // Arrange
+        var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        var expectedValues = new[] {1, 1, 2, 3, 4, 4};
+        var resultValues = new List<int>(expectedValues.Length);
+
+        // Act
+        var resultList = _11_MergeTwoSortedLists.MergeTwoLists(list1, list2);
+        while (resultList is { })
+        {
+            resultValues.Add(resultList.val);
+            resultList = resultList.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedValues, resultValues.ToArray());
+    }
+
+    [Fact]
+    public void MergeTwoLists_ShouldReturnNull_WhenGivenListsAreNull()
+    {
+        // Arrange
+        ListNode? list1 = null;
+        ListNode? list2 = null;
+
+        // Act
+        var result = _11_MergeTwoSortedLists.MergeTwoLists(list1, list2);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void MergeTwoLists_ShouldReturnListOfExpectedShape_WhenOneListIsNullAndOtherHasOneElement()
+    {
+        // Arrange
+        const int expectedValue = 0;
+        ListNode? list1 = null;
+        var list2 = new ListNode(expectedValue);
+
+        // Act
+        var resultList = _11_MergeTwoSortedLists.MergeTwoLists(list1, list2);
+
+        // Assert
+        Assert.NotNull(resultList);
+        Assert.Equal(expectedValue, resultList.val);
+        Assert.Null(resultList.next);
+    }
+}
