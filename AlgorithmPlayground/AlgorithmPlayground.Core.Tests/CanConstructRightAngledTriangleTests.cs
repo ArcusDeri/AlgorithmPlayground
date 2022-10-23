9f23301b26451d3e4dab1c5f4be584ffa845b3d1
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-23 17:44:43 +0200

    Implement algorithm for checking whether a right-angled triangle can be constructed with given array of side lengths

---
+++ AlgorithmPlayground/AlgorithmPlayground.Core.Tests/CanConstructRightAngledTriangleTests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmPlayground.Core.Algorithms;
+using Xunit;
+
+namespace AlgorithmPlayground.Core.Tests;
+
+public class CanConstructRightAngledTriangleTests
+{
+    [Theory]
+    [InlineData(false, new[] {1f})]
+    [InlineData(false, new[] {1f, 2f})]
+    [InlineData(false, new[] {1f, 1f, 20f})]
+    [InlineData(false, new[] {1f, 1f, 20f, 10f, 15f})]
+    [InlineData(true, new[] {12f, 5f, 13f})]
+    public void CanConstruct_ShouldReturnExpectedResult_ForGivenSides(bool expectedResult, float[] sides)
+    {
+        // Arrange
+        // Act
+        var result = CanConstructRightAngledTriangle.CanConstruct(sides);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.Core/Algorithms/CanConstructRightAngledTriangle.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgorithmPlayground.Core.Algorithms;
+
+public class CanConstructRightAngledTriangle
+{
+    public static bool CanConstruct(float[] sides)
+    {
+        if (sides.Length < 3)
+        {
+            return false;
+        }
+
+        var dict = new Dictionary<float, float>();
+        foreach (var side in sides)
+        {
+            var sideSquared = side * side;
+            dict[sideSquared] = side;
+        }
+
+        for (int i = 0; i < sides.Length; i++)
+        {
+            for (int j = i + 1; j < sides.Length; j++)
+            {
+                var squaresSum = sides[i] * sides[i] + sides[j] * sides[j];
+                if (dict.ContainsKey(squaresSum))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
