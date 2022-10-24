fb0695e46e66ad074ad5a6337864bae120789b06
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-24 19:04:21 +0200

    Solve Add Two Numbers

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/LinkedList/_12_AddTwoNumbersTests.cs
@@ -0,0 +1,91 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+using AlgorithmPlayground.LeetCode.Model;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.LinkedList;
+
+public class _12_AddTwoNumbersTests
+{
+    [Fact]
+    public void _01_AddTwoNumbers_ShouldReturnExpectedSum()
+    {
+        // Arrange
+        var expectedSum = new[] {7, 0, 8};
+        var number1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var number2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var resultSum = new int[expectedSum.Length];
+        var i = 0;
+
+        // Act
+        var result = _12_AddTwoNumbers.AddTwoNumbers(number1, number2);
+        while (result is { })
+        {
+            resultSum[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedSum, resultSum);
+    }
+
+    [Fact]
+    public void _02_AddTwoNumbers_ShouldReturnExpectedSum()
+    {
+        // Arrange
+        const int expectedSum = 0;
+        var number1 = new ListNode();
+        var number2 = new ListNode();
+
+        // Act
+        var result = _12_AddTwoNumbers.AddTwoNumbers(number1, number2);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Null(result.next);
+        Assert.Equal(expectedSum, result.val);
+    }
+
+    [Fact]
+    public void _03_AddTwoNumbers_ShouldReturnExpectedSum()
+    {
+        // Arrange
+        var expectedSum = new[] {8, 9, 9, 9, 0, 0, 0, 1};
+        var number1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        var number2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        var i = 0;
+        var resultSum = new int[expectedSum.Length];
+
+        // Act
+        var result = _12_AddTwoNumbers.AddTwoNumbers(number1, number2);
+        while (result is { })
+        {
+            resultSum[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedSum, resultSum);
+    }
+
+    [Fact]
+    public void _04_AddTwoNumbers_ShouldReturnExpectedSum()
+    {
+        // Arrange
+        var expectedSum = new[] {2, 0, 1};
+        var number1 = new ListNode(8, new ListNode(9));
+        var number2 = new ListNode(4);
+        var i = 0;
+        var resultSum = new int[expectedSum.Length];
+
+        // Act
+        var result = _12_AddTwoNumbers.AddTwoNumbers(number1, number2);
+        while (result is { })
+        {
+            resultSum[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedSum, resultSum);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_12_AddTwoNumbers.cs
@@ -0,0 +1,54 @@
+﻿using AlgorithmPlayground.LeetCode.Model;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+
+public static class _12_AddTwoNumbers
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1228/
+    /// </summary>
+    public static ListNode? AddTwoNumbers(ListNode? l1, ListNode? l2)
+    {
+        ListNode? prev = null;
+        var overflow = 0;
+
+        while (l1 is {} && l2 is {})
+        {
+            var subSum = l1.val + l2.val + overflow;
+            overflow = subSum / 10;
+            prev = new ListNode(subSum % 10, prev);
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        var leftNode = l1 ?? l2;
+        while (leftNode is { })
+        {
+            var subSum = leftNode.val + overflow;
+            overflow = subSum / 10;
+            prev = new ListNode(subSum % 10, prev);
+            leftNode = leftNode.next;
+        }
+
+        if (overflow > 0)
+        {
+            prev = new ListNode(overflow, prev);
+        }
+
+        return Reverse(prev);
+    }
+
+    private static ListNode? Reverse(ListNode? head)
+    {
+        ListNode? newHead = null;
+        while (head is {})
+        {
+            var subList = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = subList;
+        }
+
+        return newHead;
+    }
+}
