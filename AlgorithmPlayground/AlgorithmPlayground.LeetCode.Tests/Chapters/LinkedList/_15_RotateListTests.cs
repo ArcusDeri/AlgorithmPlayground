3b75d8c305fafca20584f2c83e561296bcfdc2b2
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-29 18:58:20 +0200

    Solve Rotate List

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/LinkedList/_15_RotateListTests.cs
@@ -0,0 +1,96 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+using AlgorithmPlayground.LeetCode.Model;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.LinkedList;
+
+public class _15_RotateListTests
+{
+    [Fact]
+    public void _01_RotateRight_ShouldReturnListOfExpectedShape()
+    {
+        // Arrange
+        var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        const int k = 2;
+        var expectedValues = new[] {4, 5, 1, 2, 3};
+        var resultValues = new int[expectedValues.Length];
+        var i = 0;
+
+        // Act
+        var result = _15_RotateList.RotateRight(head, k);
+        while (result is {})
+        {
+            resultValues[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedValues, resultValues);
+    }
+
+    [Fact]
+    public void _02_RotateRight_ShouldReturnListOfExpectedShape()
+    {
+        // Arrange
+        var head = new ListNode(0, new ListNode(1, new ListNode(2)));
+        const int k = 4;
+        var expectedValues = new[] {2, 0, 1};
+        var resultValues = new int[expectedValues.Length];
+        var i = 0;
+
+        // Act
+        var result = _15_RotateList.RotateRight(head, k);
+        while (result is {})
+        {
+            resultValues[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedValues, resultValues);
+    }
+
+    [Fact]
+    public void _03_RotateRight_ShouldReturnListOfExpectedShape()
+    {
+        // Arrange
+        var head = new ListNode(1);
+        const int k = 0;
+        var expectedValues = new[] {1};
+        var resultValues = new int[expectedValues.Length];
+        var i = 0;
+
+        // Act
+        var result = _15_RotateList.RotateRight(head, k);
+        while (result is {})
+        {
+            resultValues[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedValues, resultValues);
+    }
+
+    [Fact]
+    public void _04_RotateRight_ShouldReturnListOfExpectedShape()
+    {
+        // Arrange
+        var head = new ListNode(1);
+        const int k = 1;
+        var expectedValues = new[] {1};
+        var resultValues = new int[expectedValues.Length];
+        var i = 0;
+
+        // Act
+        var result = _15_RotateList.RotateRight(head, k);
+        while (result is {})
+        {
+            resultValues[i++] = result.val;
+            result = result.next;
+        }
+
+        // Assert
+        Assert.Equal(expectedValues, resultValues);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/LinkedList/_15_RotateList.cs
@@ -0,0 +1,57 @@
+﻿using AlgorithmPlayground.LeetCode.Model;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.LinkedList;
+
+public static class _15_RotateList
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1295/
+    /// </summary>
+    public static ListNode? RotateRight(ListNode? head, int k)
+    { 
+        var listLength = 0;
+        var front = head;
+
+        // resolve list length
+        while (front is {})
+        {
+            listLength++;
+            front = front.next;
+        }
+
+        if (listLength == 0)
+        {
+            return null;
+        }
+
+        k = k >= listLength ? k % listLength : k;
+        if (k == 0)
+        {
+            return head;
+        }
+
+        front = head;
+        var distance = 0;
+        // move one pointer ahead to maintain the distance
+        while (distance < k)
+        {
+            front = front.next;
+            distance++;
+        }
+
+        var back = head;
+        // find node that has to be detached...
+        while (front?.next is {})
+        {
+            back = back.next;
+            front = front.next;
+        }
+
+        // ...and move it to the beginning
+        var newHead = back.next;
+        back.next = null;
+        front.next = head;
+
+        return newHead;
+    }
+}
