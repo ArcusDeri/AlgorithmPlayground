8fffb2450977632c1a5c2cb85368ab68737ee52b
Marcin Michniak <marcin.michniak@outlook.com>
2022-10-30 22:07:10 +0100

    Solve Reverse String recursively

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_01_ReverseStringTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._01_PrincipleOfRecursion;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1
+{
+    public class _01_ReverseStringTests
+    {
+        [Theory]
+        [InlineData(new[] {'o', 'l', 'l', 'e', 'h' }, new[] {'h', 'e', 'l', 'l', 'o'})]
+        [InlineData(new[] {'h', 'a', 'n', 'n', 'a', 'H' }, new[] {'H', 'a', 'n', 'n', 'a', 'h'})]
+        public void _01_ReverseString_ShouldReturnExpectedResult(char[] expectedResult, char[] str)
+        {
+            // Arrange
+            // Act
+            _01_ReverseString.ReverseString(str);
+
+            // Assert
+            Assert.Equal(expectedResult, str);
+        }
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_01_PrincipleOfRecursion/_01_ReverseString.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._01_PrincipleOfRecursion
+{
+    public static class _01_ReverseString
+    {
+        public static void ReverseString(char[] s)
+        {
+            Helper(0, s.Length - 1, s);
+        }
+
+        private static void Helper(int left, int right, char[] s)
+        {
+            if (left < right)
+            {
+                (s[left], s[right]) = (s[right], s[left]);
+                Helper(left + 1, right - 1, s);
+            }
+        }
+    }
+}
