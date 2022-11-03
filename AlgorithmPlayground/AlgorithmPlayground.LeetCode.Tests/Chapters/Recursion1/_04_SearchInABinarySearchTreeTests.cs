5cbc15ad21808798269b486ad094fc69a66f158d
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-03 18:17:22 +0100

    Solve Search in a Binary Search Tree recursively

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_04_SearchInABinarySearchTreeTests.cs
@@ -0,0 +1,39 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._02_RecurrenceRelation;
+using AlgorithmPlayground.LeetCode.Model;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _04_SearchInABinarySearchTreeTests
+{
+    [Fact]
+    public void _01_SearchBST_ShouldReturnExpectedTreeNode_WhenTreeContainsSearchedValue()
+    {
+        // Arrange
+        const int searchedValue = 2;
+        var expectedNode = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        var root = new TreeNode(4, expectedNode, new TreeNode(7));
+
+        // Act
+        var resultNode = _04_SearchInABinarySearchTree.SearchBST(root, searchedValue);
+
+        // Assert
+        Assert.NotNull(resultNode);
+        Assert.Same(expectedNode, resultNode);
+    }
+
+    [Fact]
+    public void _02_SearchBST_ShouldReturnExpectedTreeNode_WhenTreeDoesNotContainSearchedValue()
+    {
+        // Arrange
+        const int searchedValue = 5;
+        var subTree = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        var root = new TreeNode(4, subTree, new TreeNode(7));
+
+        // Act
+        var resultNode = _04_SearchInABinarySearchTree.SearchBST(root, searchedValue);
+
+        // Assert
+        Assert.Null(resultNode);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_02_RecurrenceRelation/_04_SearchInABinarySearchTree.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable InconsistentNaming
+using AlgorithmPlayground.LeetCode.Model;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._02_RecurrenceRelation;
+
+public static class _04_SearchInABinarySearchTree
+{
+    public static TreeNode? SearchBST(TreeNode? root, int val)
+    {
+        if (root is null || root.val == val)
+        {
+            return root;
+        }
+
+        return SearchBST(val < root.val ? root.left : root.right, val);
+    }
+}
