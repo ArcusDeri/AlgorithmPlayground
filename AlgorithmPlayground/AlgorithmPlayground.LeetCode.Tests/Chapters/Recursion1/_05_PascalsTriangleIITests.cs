04f7d47f3ad9dc48231303b2d197bb12131d49c5
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-05 14:10:53 +0100

    Solve Pascal's Triangle II

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_05_PascalsTriangleIITests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._02_RecurrenceRelation;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _05_PascalsTriangleIITests
+{
+    [Theory]
+    [InlineData(new[] {1, 3, 3, 1}, 3)]
+    [InlineData(new[] {1}, 0)]
+    [InlineData(new[] {1, 4, 6, 4, 1}, 4)]
+    [InlineData(new[] {1, 5, 10, 10, 5, 1}, 5)]
+    [InlineData(new[] {1, 1}, 1)]
+    public void _01_GetRow_ShouldReturnExpectedResult_ForFourthRow(int[] expectedResult, int rowIndex)
+    {
+        // Arrange
+        // Act
+        var result = _05_PascalsTriangleII.GetRow(rowIndex);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_02_RecurrenceRelation/_05_PascalsTriangleII.cs
@@ -0,0 +1,41 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._02_RecurrenceRelation;
+
+public static class _05_PascalsTriangleII
+{
+    private static readonly Dictionary<(int column, int row), int> MemoTable = new ();
+
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/251/scenario-i-recurrence-relation/3234/
+    /// </summary>
+    public static IList<int> GetRow(int rowIndex)
+    {
+        var result = new List<int>();
+        var i = 0;
+
+        while (i <= rowIndex)
+        {
+            result.Add(GetPascalTriangleNumber(i++, rowIndex));
+        }
+
+        MemoTable.Clear();
+        return result;
+    }
+
+    private static int GetPascalTriangleNumber(int column, int row)
+    {
+        if (MemoTable.ContainsKey((column, row)))
+        {
+            return MemoTable[(column, row)];
+        }
+
+        if (column == 0 || row == column)
+        {
+            return 1;
+        }
+
+        var result = GetPascalTriangleNumber(column - 1, row - 1) + GetPascalTriangleNumber(column, row - 1);
+        MemoTable[(column, row)] = result;
+
+        return result;
+    }
+}
