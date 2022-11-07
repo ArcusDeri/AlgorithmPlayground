0172147286f4d893d1b919aac0b09fe8dbeeff0b
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-07 17:29:47 +0100

    Solve Fibonacci Number recursively, with memoization

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_06_FibonacciNumberTests.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._03_Memoization;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _06_FibonacciNumberTests
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(0, 0)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 3)]
+    [InlineData(7, 13)]
+    [InlineData(10, 55)]
+    public void _01_Fib_ShouldReturnExpectedResult_ForNthSequenceElement(int n, int expectedResult)
+    {
+        // Arrange
+        var solver = new _06_FibonacciNumber();
+
+        // Act
+        var result = solver.Fib(n);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_03_Memoization/_06_FibonacciNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._03_Memoization;
+
+public class _06_FibonacciNumber
+{
+    private readonly Dictionary<int, int> _memoTable = new ();
+
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/1661/
+    /// </summary>
+    public int Fib(int n)
+    {
+        if (_memoTable.ContainsKey(n))
+        {
+            return _memoTable[n];
+        }
+
+        if (n == 0 || n == 1)
+        {
+            _memoTable[n] = n;
+            return n;
+        }
+
+        var result = Fib(n - 2) + Fib(n - 1);
+        _memoTable[n] = result;
+        return result;
+    }
+}
