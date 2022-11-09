c50126d5ccd03267fc190e4fe85b2081ac21415c
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-09 19:20:22 +0100

    Solve Climbing Stairs recursively

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_07_ClimbingStairsTests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._03_Memoization;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _07_ClimbingStairsTests
+{
+    [Theory]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(8, 5)]
+    public void ClimbStairs_ShouldReturnExpectedResult(int expectedResult, int n)
+    {
+        // Arrange
+        var solver = new _07_ClimbingStairs();
+
+        // Act
+        var result = solver.ClimbStairs(n);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_03_Memoization/_07_ClimbingStairs.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._03_Memoization;
+
+public class _07_ClimbingStairs
+{
+    private readonly Dictionary<int, int> _cache = new();
+
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/1662/
+    /// </summary>
+    public int ClimbStairs(int n)
+    {
+        if (_cache.ContainsKey(n))
+        {
+            return _cache[n];
+        }
+
+        if (n is 1 or 2)
+        {
+            _cache[n] = n;
+            return n;
+        }
+
+        var result = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+        _cache[n] = result;
+        return result;
+    }
+}
