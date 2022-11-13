b99b5240c4011b57ed271366b7d29cf92ab51a40
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-13 14:58:09 +0100

    Solve Pow(x,n)

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_09_PowXnTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1._04_ComplexityAnalysis;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _09_PowXnTests
+{
+    [Theory]
+    [InlineData(1024.0D, 2.0, 10)]
+    [InlineData(9.26100D, 2.1, 3)]
+    [InlineData(0.25000D, 2.0, -2)]
+    public void _01_MyPowXn_ShouldReturnExpectedResult(double expectedResult, double x, int n)
+    {
+        // Arrange
+        // Act
+        var result = _09_PowXn.MyPow(x, n);
+
+        // Assert
+        Assert.Equal(expectedResult, Math.Round(result, 3) );
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_04_ComplexityAnalysis/_09_PowXn.cs
@@ -0,0 +1,32 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1._04_ComplexityAnalysis;
+
+public static class _09_PowXn
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/256/complexity-analysis/2380/
+    /// </summary>
+    public static double MyPow(double x, int n)
+    {
+        if (n < 0)
+        {
+            return 1 / x * MyPow(1 / x, -(n + 1));
+        }
+
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        if (n == 1)
+        {
+            return x;
+        }
+
+        if (n % 2 == 0)
+        {
+            return MyPow(x * x, n / 2);
+        }
+
+        return x * MyPow(x * x, n / 2);
+    }
+}
