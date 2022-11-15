5a72f3d0d4e0570e9defce4804870fe14a2e37f2
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-15 23:04:39 +0100

    Add non optimal solution for K-th Symbol in Grammar

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/Recursion1/_10_KthSymbolInGrammarTests.cs
@@ -0,0 +1,22 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.Recursion1;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.Recursion1;
+
+public class _10_KthSymbolInGrammarTests
+{
+    [Theory]
+    [InlineData(0, 1, 1)]
+    [InlineData(0, 2, 1)]
+    [InlineData(1, 2, 2)]
+    [InlineData(1, 3, 3)]
+    public void _01_KthGrammar_ShouldReturnExpectedResult(int expectedResult, int n, int k)
+    {
+        // Arrange
+        // Act
+        var result = _10_KthSymbolInGrammar.KthGrammar(n, k);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/Recursion1/_10_KthSymbolInGrammar.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.Recursion1;
+
+public static class _10_KthSymbolInGrammar
+{
+    public static int KthGrammar(int n, int k)
+    {
+        return GetNthRow(new[] {0}, n)[k - 1];
+    }
+
+    private static int[] GetNthRow(int[] row, int n)
+    {
+        if (row.Length == Math.Pow(2, n - 1))
+        {
+            return row;
+        }
+
+        var initialRowLength = row.Length;
+        var arr = row.Concat(row).ToArray();
+        for (int i = initialRowLength; i < arr.Length; i++)
+        {
+            arr[i] = arr[i] == 0 ? 1 : 0;
+        }
+        return GetNthRow(arr, n);
+    }
+}
