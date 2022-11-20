5ca734303288df17d91dcdf1e629bf35f5862e81
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-20 16:57:41 +0100

    Solve Find Pivot Index

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_01_FindPivotIndexTests.cs
@@ -0,0 +1,22 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _01_FindPivotIndexTests
+{
+    [Theory]
+    [InlineData(3, new[] {1, 7, 3, 6, 5, 6})]
+    [InlineData(-1, new[] {1, 2, 3})]
+    [InlineData(0, new[] {2, -1, 1})]
+    [InlineData(6, new[] {2, -2, 3, -3, 5, -5, 9})]
+    public void PivotIndex_ShouldReturnExpectedResult(int expectedResult, int[] nums)
+    {
+        // Arrange
+        // Act
+        var result = _01_FindPivotIndex.PivotIndex(nums);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/IntroductionToArray/_01_FindPivotIndex.cs
@@ -0,0 +1,36 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+
+public static class _01_FindPivotIndex
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+    /// </summary>
+    public static int PivotIndex(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0] == 0 ? 0 : -1;
+        }
+
+        var rightSum = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        if (rightSum == 0)
+        {
+            return 0;
+        }
+
+        var leftSum = 0;
+        var pivot = 1;
+        while (leftSum != rightSum && pivot < nums.Length)
+        {
+            leftSum += nums[pivot - 1];
+            rightSum -= nums[pivot++];
+        }
+
+        return leftSum == rightSum ? pivot - 1 : -1;
+    }
+}
