d7eadfd17c76f3fa59e3a0532198666290080031
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-21 18:15:50 +0100

    Solve Largest Number at least Twice of Others

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_02_LargestNumberAtLeastTwiceOfOthersTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _02_LargestNumberAtLeastTwiceOfOthersTests
+{
+    [Theory]
+    [InlineData(1, new[] {3, 6, 1, 0})]
+    [InlineData(-1, new[] {1, 2, 3, 4})]
+    [InlineData(0, new[] {1,0})]
+    public void DominantIndex_ShouldReturnExpectedResult(int expectedResult, int[] nums)
+    {
+        // Arrange
+        // Act
+        var result = _02_LargestNumberAtLeastTwiceOfOthers.DominantIndex(nums);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/IntroductionToArray/_02_LargestNumberAtLeastTwiceOfOthers.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+
+public static class _02_LargestNumberAtLeastTwiceOfOthers
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
+    /// </summary>
+    public static int DominantIndex(int[] nums)
+    {
+        var secondMaxValue = 0;
+        var maxValue = 0;
+        var maxIndex = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > maxValue)
+            {
+                secondMaxValue = maxValue;
+                maxIndex = i;
+                maxValue = nums[i];
+            }
+            else if (nums[i] > secondMaxValue)
+            {
+                secondMaxValue = nums[i];
+            }
+        }
+        return maxValue >= 2 * secondMaxValue ? maxIndex : -1;
+    }
+}
