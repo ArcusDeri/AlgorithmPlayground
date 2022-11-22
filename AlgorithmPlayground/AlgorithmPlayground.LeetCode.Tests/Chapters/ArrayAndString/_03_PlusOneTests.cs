a5ff350fe45ff1307afbcf6c105774d9baaf95b9
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-22 16:58:02 +0100

    Solve Plus One

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_03_PlusOneTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _03_PlusOneTests
+{
+    [Theory]
+    [InlineData(new[] {1, 2, 4}, new[] {1, 2, 3})]
+    [InlineData(new[] {1, 0}, new[] {9})]
+    [InlineData(new[] {4, 3, 2, 2}, new[] {4, 3, 2, 1})]
+    public void PlusOne_ShouldReturnExpectedResult(int[] expectedResult, int[] digits)
+    {
+        // Arrange
+        // Act
+        var result = _03_PlusOne.PlusOne(digits);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/IntroductionToArray/_03_PlusOne.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString.IntroductionToArray;
+
+public static class _03_PlusOne
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1148/
+    /// </summary>
+    public static int[] PlusOne(int[] digits)
+    {
+        var carry = 0;
+        digits[digits.Length - 1] += 1;
+
+        for (var i = digits.Length - 1; i > -1; i--)
+        {
+            digits[i] += carry;
+            if (digits[i] > 9)
+            {
+                digits[i] %= 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+        }
+        
+        return carry == 0 ? digits : (new[] {1}).Concat(digits).ToArray();
+    }
+}
