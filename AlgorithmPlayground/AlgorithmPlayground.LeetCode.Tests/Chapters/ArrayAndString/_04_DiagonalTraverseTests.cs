8c879bee11b35e8ca0fbcd77c0d913eeac2013a5
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-25 23:28:29 +0100

    Solve Diagonal Traverse

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_04_DiagonalTraverseTests.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _04_DiagonalTraverseTests
+{
+    [Fact]
+    public void _01_FindDiagonalOrder_ReturnsExpectedResult_For3x3Matrix()
+    {
+        // Arrange
+        var matrix = new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}};
+        var expectedResult = new[] {1, 2, 4, 7, 5, 3, 6, 8, 9};
+
+        // Act
+        var result = _04_DiagonalTraverse.FindDiagonalOrder(matrix);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void _02_FindDiagonalOrder_ReturnsExpectedResult_For2x2Matrix()
+    {
+        // Arrange
+        var matrix = new[] {new[] {1, 2}, new[] {3, 4}};
+        var expectedResult = new[] {1, 2, 3, 4};
+
+        // Act
+        var result = _04_DiagonalTraverse.FindDiagonalOrder(matrix);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/_02_IntroductionTo2DArray/_04_DiagonalTraverse.cs
@@ -0,0 +1,63 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+
+public static class _04_DiagonalTraverse
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+    /// </summary>
+    public static int[] FindDiagonalOrder(int[][] mat)
+    {
+        if (mat == null || mat.Length == 0)
+        {
+            return new int[0];
+        }
+
+        var row = 0;
+        var col = 0;
+        var m = mat.Length;
+        var n = mat[0].Length;
+        var result = new int[m * n];
+
+        for (var resultCount = 0; resultCount < m * n; resultCount++)
+        {
+            result[resultCount] = mat[row][col];
+            
+            // direction is up when row + col is even
+            if ((row + col) % 2 == 0)
+            {
+                if (col == n - 1)
+                {
+                    row++;
+                }
+                else if (row == 0)
+                {
+                    col++;
+                }
+                else
+                {
+                    row--;
+                    col++;
+                }
+            }
+            else
+            {
+                
+                if (row == m - 1)
+                {
+                    col++;
+                }
+                else if (col == 0)
+                {
+                    row++;
+                }
+                else
+                {
+                    row++;
+                    col--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
