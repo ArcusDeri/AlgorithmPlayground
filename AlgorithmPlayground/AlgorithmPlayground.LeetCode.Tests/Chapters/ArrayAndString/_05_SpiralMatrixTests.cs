0854ac392f4148e0efe0b3cfe64246d0a735c105
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-26 20:23:31 +0100

    Solve Spiral Matrix

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_05_SpiralMatrixTests.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _05_SpiralMatrixTests
+{
+    [Fact]
+    public void SpiralOrder_ShouldReturnExpectedResult_ForSquareMatrix()
+    {
+        // Arrange
+        var matrix = new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}};
+        var expectedResult = new[] {1, 2, 3, 6, 9, 8, 7, 4, 5};
+
+        // Act
+        var result = _05_SpiralMatrix.SpiralOrder(matrix);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void SpiralOrder_ShouldReturnExpectedResult_ForSRectangleMatrix()
+    {
+        // Arrange
+        var matrix = new[] {new[] {1, 2, 3, 4}, new[] {5, 6, 7, 8}, new[] {9, 10, 11, 12}};
+        var expectedResult = new[] {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+
+        // Act
+        var result = _05_SpiralMatrix.SpiralOrder(matrix);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/_02_IntroductionTo2DArray/_05_SpiralMatrix.cs
@@ -0,0 +1,69 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+
+public static class _05_SpiralMatrix
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1168/
+    /// </summary>
+    public static int[] SpiralOrder(int[][] matrix)
+    {
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+        var result = new int[m * n];
+
+        var rightLimit = n - 1;
+        var leftLimit = 0;
+        var upLimit = 1;
+        var downLimit = m - 1;
+
+        var row = 0;
+        var col = 0;
+        var dir = n > 1 ? 1 : 2;
+        for (var resultCount = 0; resultCount < m * n; resultCount++)
+        {
+            result[resultCount] = matrix[row][col];
+            if (resultCount == m * n)
+            {
+                return result;
+            }
+
+            switch (dir)
+            {
+                case 1: // rightwards
+                    col++;
+                    if (col == rightLimit)
+                    {
+                        rightLimit--;
+                        dir++;
+                    }
+                    break;
+                case 2: // downwards
+                    row++;
+                    if (row == downLimit)
+                    {
+                        downLimit--;
+                        dir++;
+                    }
+                    break;
+                case 3: // leftwards
+                    col--;
+                    if (col == leftLimit)
+                    {
+                        leftLimit++;
+                        dir++;
+                    }
+                    break;
+                case 4: // upwards
+                    row--;
+                    if (row == upLimit)
+                    {
+                        upLimit++;
+                        dir = 1;
+                    }
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
