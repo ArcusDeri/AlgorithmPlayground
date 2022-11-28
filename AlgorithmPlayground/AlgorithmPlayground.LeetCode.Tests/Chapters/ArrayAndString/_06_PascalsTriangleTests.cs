96e1be1ba56bad84836121a4882106104010fcb9
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-28 20:04:31 +0100

    Solve Pascal's Triangle

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_06_PascalsTriangleTests.cs
@@ -0,0 +1,39 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _06_PascalsTriangleTests
+{
+    [Fact]
+    public void _01_Generate_ShouldReturnExpectedResult_ForRowOne()
+    {
+        // Arrange
+        var expectedResult = new[] {new[] {1}};
+        const int row = 1;
+
+        // Act
+        var result = _06_PascalsTriangle.Generate(row)
+            .Select(s => s.ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.Equal(expectedResult,result);
+    }
+
+    [Fact]
+    public void _02_Generate_ShouldReturnExpectedResult_ForRowFive()
+    {
+        // Arrange
+        var expectedResult = new[] {new[]{1}, new[]{1, 1}, new[]{1, 2, 1}, new[]{1, 3, 3, 1}, new[]{1, 4, 6, 4, 1}};
+        const int row = 5;
+
+        // Act
+        var result = _06_PascalsTriangle.Generate(row)
+            .Select(s => s.ToArray())
+            .ToArray();
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/_02_IntroductionTo2DArray/_06_PascalsTriangle.cs
@@ -0,0 +1,43 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._02_IntroductionTo2DArray;
+
+public static class _06_PascalsTriangle
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1170/
+    /// </summary>
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        var rows = new List<IList<int>> {new List<int> {1}};
+
+        if (numRows == 1)
+        {
+            return rows;
+        }
+
+        rows.Add(new[] {1, 1});
+
+        if (numRows == 2)
+        {
+            return rows;
+        }
+
+        for (var i = 2; i < numRows; i++)
+        {
+            var row = new List<int>();
+            for (int j = 0; j < i + 1; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    row.Add(1);
+                    continue;
+                }
+
+                row.Add(rows[i - 1][j - 1] + rows[i - 1][j]);
+            }
+
+            rows.Add(row);
+        }
+
+        return rows;
+    }
+}
