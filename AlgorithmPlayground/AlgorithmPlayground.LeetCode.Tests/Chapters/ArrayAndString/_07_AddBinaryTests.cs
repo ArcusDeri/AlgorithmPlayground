2b87490d8cda46dcf0ffe504b01d09154b0e522d
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-29 19:06:07 +0100

    Solve Add Binary

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_07_AddBinaryTests.cs
@@ -0,0 +1,20 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._03_IntroductionToString;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _07_AddBinaryTests
+{
+  [Theory]
+  [InlineData("100", "11", "1")]
+  [InlineData("10101", "1010", "1011")]
+  public void _01_AddBinary_ShouldReturnExpectedResult(string expectedResult, string a, string b)
+  {
+    // Arrange
+    // Act
+    var result = _07_AddBinary.AddBinary(a, b);
+
+    // Assert
+    Assert.Equal(expectedResult, result);
+  }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/_03_IntroductionToString/_07_AddBinary.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._03_IntroductionToString;
+
+public static class _07_AddBinary
+{
+    public static string AddBinary(string a, string b)
+    {
+        var resultBuilder = new StringBuilder();
+        var aIndex = a.Length - 1;
+        var bIndex = b.Length - 1;
+        var carry = 0;
+
+        while (aIndex >= 0 || bIndex >= 0 || carry > 0)
+        {
+            var valA = aIndex >= 0 ? int.Parse(a[aIndex--].ToString()) : 0;
+            var valB = bIndex >= 0 ? int.Parse(b[bIndex--].ToString()) : 0;
+            var valC = valA + valB + carry;
+
+            if (valC == 0)
+            {
+                resultBuilder.Insert(0, '0');
+            }
+            else if (valC == 1)
+            {
+                carry = 0;
+                resultBuilder.Insert(0, '1');
+            }
+            else if (valC == 2)
+            {
+                carry = 1;
+                resultBuilder.Insert(0, '0');
+            }
+            else if (valC == 3)
+            {
+                carry = 1;
+                resultBuilder.Insert(0, '1');
+            }
+        }
+
+        return resultBuilder.ToString();
+    }
+}
