288da81fc8b0c90efc4cfddd8d0c0d040069f2f1
Marcin Michniak <marcin.michniak@outlook.com>
2022-11-30 19:21:08 +0100

    Solve Implement strStr()

---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode.Tests/Chapters/ArrayAndString/_08_ImplementStrStrTests.cs
@@ -0,0 +1,20 @@
+﻿using AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._03_IntroductionToString;
+using Xunit;
+
+namespace AlgorithmPlayground.LeetCode.Tests.Chapters.ArrayAndString;
+
+public class _08_ImplementStrStrTests
+{
+    [Theory]
+    [InlineData(0, "sadbutsad", "sad")]
+    [InlineData(-1, "leetcode", "leeto")]
+    public void _01_StrStr_ShouldReturnExpectedResult(int expectedResult, string haystack, string needle)
+    {
+        // Arrange
+        // Act
+        var result = _08_ImplementStrStr.StrStr(haystack, needle);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmPlayground/AlgorithmPlayground.LeetCode/Chapters/ArrayAndString/_03_IntroductionToString/_08_ImplementStrStr.cs
@@ -0,0 +1,45 @@
+﻿namespace AlgorithmPlayground.LeetCode.Chapters.ArrayAndString._03_IntroductionToString;
+
+public static class _08_ImplementStrStr
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        var hLength = haystack.Length;
+        var nLength = needle.Length;
+        var result = -1;
+        if (hLength < nLength)
+        {
+            return result;
+        }
+
+        for (int i = 0; i < hLength; i++)
+        {
+            if (hLength - i < nLength)
+            {
+                return -1;
+            }
+
+            if (haystack[i] != needle[0])
+            {
+                continue;
+            }
+
+            result = i;
+            for (int j = 0, k = i; j < nLength; j++, k++)
+            {
+                if (haystack[k] != needle[j])
+                {
+                    result = -1;
+                    break;
+                }
+            }
+
+            if (result != -1)
+            {
+                return result;
+            }
+        }
+
+        return result;
+    }
+}
